555599b6e87acb2ca1d312727f661194275356ca
mahnoor643 <mahnoort643@gmail.com>
2024-11-10 15:49:07 +0500

    validation  done

--- Routes/TodoRoutes.js
+++ Routes/TodoRoutes.js
@@ -1,78 +1,103 @@
-const express=require("express");
-const router=express.Router();
-const Todo=require("../Models/Todo");
+const express = require("express");
+const router = express.Router();
+const Todo = require("../Models/Todo");
+const { body, validationResult } = require("express-validator");
 
 //create todo endpoint
-router.post("/create",async(req,res)=>{
-    try {
-        const {title,description,isComplete}=req.body;
-        const newTodo=new Todo({title,description,isComplete});
-        const savedTodo=await newTodo.save();
-        res.status(201).json(savedTodo);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error: error.message});
-    }
-});
+router.post("/create",
+    [
+        body("title")
+            .notEmpty()
+            .isString()
+            .isLength({ max: 100 })
+            .withMessage("Title is required")
+    ],
+    async (req, res) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            res.status(400).json({ error: error });
+        }
+        try {
+            const { title, description, isComplete } = req.body;
+            const newTodo = new Todo({ title, description, isComplete });
+            const savedTodo = await newTodo.save();
+            res.status(201).json(savedTodo);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: error.message });
+        }
+    });
 
 //get all tasks
-router.get("/getAll",async(req,res)=>{
+router.get("/getAll", async (req, res) => {
     try {
-        const task=await Todo.find();
-        if(task==null){
-            res.status(404).json({error: "Not  Found"});
+        const task = await Todo.find();
+        if (task == null) {
+            res.status(404).json({ error: "Not  Found" });
         }
         res.status(200).json(task);
     } catch (error) {
-        res.status(500).json({error: error.message});
+        res.status(500).json({ error: error.message });
     }
 });
 
 //get tasks by id
-router.get("/:id",async(req,res)=>{
+router.get("/:id", async (req, res) => {
     try {
-        const {id}=req.params;
-        const task=await Todo.findOne({_id:id});
-        if(!task){
-            return res.status(404).json({error: "task not found"});
+        const { id } = req.params;
+        const task = await Todo.findOne({ _id: id });
+        if (!task) {
+            return res.status(404).json({ error: "task not found" });
         }
         res.status(200).json(task);
     } catch (error) {
-        res.status(500).json({error:error.message})
+        res.status(500).json({ error: error.message })
     }
 });
 
 //update task by id
-router.put("/:id",async(req,res)=>{
-    try {
-        const {id}=req.params;
-        const {title,description,isComplete}=req.body;
-        const updatedTask=await Todo.findByIdAndUpdate(
-            {_id:id},
-            {title,description,isComplete},
-            {new:true}
-        );
-        if(!updatedTask){
-            return res.status(404).json({error:error.message});
+router.put("/:id",
+    [
+        body("title")
+            .notEmpty()
+            .isString()
+            .isLength({ max: 100 })
+            .withMessage("Title is required")
+    ],
+    async (req, res) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            res.status(400).json({ error: error });
         }
-        return res.status(200).json(updatedTask);
-    } catch (error) {
-        res.status(500).json({error:error.message});
-    }
-});
+        try {
+            const { id } = req.params;
+            const { title, description, isComplete } = req.body;
+            const updatedTask = await Todo.findByIdAndUpdate(
+                { _id: id },
+                { title, description, isComplete },
+                { new: true }
+            );
+            if (!updatedTask) {
+                return res.status(404).json({ error: error.message });
+            }
+            return res.status(200).json(updatedTask);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    });
 
 //delete task
-router.delete("/:id",async(req,res)=>{
+router.delete("/:id", async (req, res) => {
     try {
-        const {id}=req.params;
-        const deleteTask=await Todo.findByIdAndDelete({_id:id});
-        if(!deleteTask){
-            return res.status(404).json({error:"task not found"});
+        const { id } = req.params;
+        const deleteTask = await Todo.findByIdAndDelete({ _id: id });
+        if (!deleteTask) {
+            return res.status(404).json({ error: "task not found" });
         }
-        return res.status(200).json({success: "Task deleted successfully"});
+        return res.status(200).json({ success: "Task deleted successfully" });
     } catch (error) {
-        res.status(500).json({error:error.message});
+        res.status(500).json({ error: error.message });
     }
 });
 
-module.exports=router;
+module.exports = router;
